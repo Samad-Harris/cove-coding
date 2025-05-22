e084b7e0de770d75361e95c26da0e295679d19c6
Samad Harris <samadharris430@gmail.com>
2025-05-22 15:50:35 -0400

    :wrench: update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,22 @@
-
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
+
   framework: {
-    name: '@storybook/react',
+    name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
-  staticDirs: ['../public'],
+
+  docs: {},
+
+  staticDirs: ["../public"]
 };
 
 export default config;
